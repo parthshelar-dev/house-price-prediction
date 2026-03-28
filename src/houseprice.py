a83bc9bbf4370212f612349c12b9dba45956279f
Parth Shelar <parthshelar121005@gmail.com>
2026-03-28 20:01:07 +0530

    Update houseprice.py

--- src/houseprice.py
+++ src/houseprice.py
@@ -41,7 +41,7 @@ plt.show()
 
 # Split dataset
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.3, random_state=100
+    X, y, test_size=0.2, random_state=42
 )
 
 # Data cleaning
